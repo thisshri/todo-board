6940721c6137c5140fcebd91bc381f35ddd3e7e2
Shrikant <thisshri@gmail.com>
2020-12-12 15:05:23 +0530

    Add style to card component

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -4,30 +4,33 @@ import {
   BOARD_TYPES,
 } from 'utils';
 
+import './styles.scss';
+
 const Card = ({
   taskTitle,
   boardName,
 }) => {
   return (
-    <>
-    <div>
-      <p>
-        {taskTitle}
-      </p>
-      <button>
-        delete
-      </button>
+    <div className="card">
+      <div className="header">
+        <span>
+          {taskTitle}
+        </span>
+        <button>
+          Delete
+        </button>
+      </div>
+      <select name="Change Status">
+        <option value="">Move to board</option>
+        {
+          BOARD_TYPES.filter(
+            option => option.value !== boardName
+          ).map(
+            option => <option value={option.value}>{option.title}</option>
+          )
+        }
+      </select>
     </div>
-    <select name="Change Status">
-      {
-        BOARD_TYPES.filter(
-          option => option.value !== boardName
-        ).map(
-          option => <option value={option.value}>{option.title}</option>
-        )
-      }
-    </select>
-    </>
   )
 }
 
